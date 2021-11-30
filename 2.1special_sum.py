7d227f06752b879181273f40e460bf43707bc752
chinashiyang <93908727+chinashiyang@users.noreply.github.com>
2021-11-30 23:21:44 +0300

    Update 2.1special_sum.py

--- 2.1special_sum.py
+++ 2.1special_sum.py
@@ -1,1 +1,7 @@
 
+#Implement a function that takes a natural number 𝑛 as an argument and
+#calculates 12 *2+ 22 *3+...+ (𝑛−1)2 *𝑛 sum. Use only one comprehension expression in your solution.
+#Asymptotical complexity of the algorithm should be 𝑂(𝑛).
+#Note. Name your function calculate_special_sum and save it into a special_sum.py file.
+#Example
+#assert calculate_special_sum ( 3 ) == 14
