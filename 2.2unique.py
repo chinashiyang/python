a232f3a94a1ae5ba62bcbebc364b688e1b8870f9
chinashiyang <93908727+chinashiyang@users.noreply.github.com>
2021-11-30 23:27:39 +0300

    Create 2.2unique.py

---
+++ 2.2unique.py
@@ -0,0 +1,9 @@
+#Implement a function that takes a sequence (a list or a tuple) and returns
+#an iterable of pairs containing a unique item and number of times the item is presented in the given
+#sequence. A pair is tuple of 2 elements. Pairs in the iterable returned may be ordered arbitrary.
+#Note. Name your function compress and save it into a unique.py file.
+#Example
+#expected_sorted = [(1 , 2 ) , (2 , 1 ) , (3 , 1)]
+#actual_sorted = sorted ( compress ([1 , 2 , 1 , 3]) )
+#assert expected_sorted == actual_sorted
+
