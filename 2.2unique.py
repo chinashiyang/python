f4350673df17aaece51091c2d747e6200a0ce149
chinashiyang <93908727+chinashiyang@users.noreply.github.com>
2021-11-30 23:44:41 +0300

    Update 2.2unique.py

--- 2.2unique.py
+++ 2.2unique.py
@@ -7,3 +7,14 @@
 #actual_sorted = sorted ( compress ([1 , 2 , 1 , 3]) )
 #assert expected_sorted == actual_sorted
 
+
+expected_sorted = ['a', 'b', 'c']
+a=(x,y)
+zipped  # Holds an iterator object
+<zip object at 0x7fa4831153c8>
+>>> type(zipped)
+<class 'zip'>
+>>> list(zipped)
+[(1, 'a'), (2, 'b'), (3, 'c')]
+return
+expected_sorted = [(a ,b),(c,d)]
